683a8a288f4e50fd2dfd1fdc50c15bb08074444a
Pavan Maganti <pavanmaganti9@gmail.com>
2020-04-18 22:57:36 +0530

    Update models.py

--- default/models.py
+++ default/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class sms(models.Model):
+	name = models.CharField(max_length=50)
+	mobile = models.IntegerField()
+	created = models.DateTimeField(auto_now_add=True)
+	
+	def __str__(self):
+		return format(self.name)
